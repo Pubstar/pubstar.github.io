727a911932721c4d9bf9a9f5b42465de41b4fc1e
Pubstar <pieterhoek@msn.com>
2022-06-05 20:17:57 +0200

    Added animations

--- snap-homepage/script.js
+++ snap-homepage/script.js
@@ -6,6 +6,9 @@ let hamburgerIcon = document.querySelector('.hamburger-icon');
 let featuresLi = document.querySelector('.features-li');
 let companyLi = document.querySelector('.company-li');
 
+let featuresMenu = document.querySelector('.features-menu');
+let companyMenu = document.querySelector('.company-menu');
+
 featuresLi.style.setProperty('--arrow', 'url(./images/icon-arrow-down.svg)');
 companyLi.style.setProperty('--arrow', 'url(./images/icon-arrow-down.svg)');
 
@@ -13,21 +16,31 @@ featuresText.style.setProperty('--arrow', 'url(./images/icon-arrow-down.svg)');
 companyText.style.setProperty('--arrow', 'url(./images/icon-arrow-down.svg)');
 
 featuresLi.addEventListener('click', () => {
-    if (document.querySelector('.features-menu').style.display == "block") {
-        document.querySelector('.features-menu').style.display = 'none';
+    if (featuresMenu.style.visibility == "visible") {
+        featuresMenu.style.visibility = 'hidden';
+        featuresMenu.style.opacity = 0;
         featuresLi.style.setProperty('--arrow', 'url(./images/icon-arrow-down.svg)');
+        featuresMenu.classList.add('shown');
     } else {
-        document.querySelector('.features-menu').style.display = 'block';
+        featuresMenu.style.visibility = 'visible';
+        companyMenu.style.visibility = 'hidden';
+        featuresMenu.style.opacity = 1;
         featuresLi.style.setProperty('--arrow', 'url(./images/icon-arrow-up.svg)');
+        featuresMenu.classList.remove('shown');
     }
 })
 companyLi.addEventListener('click', () => {
-    if (document.querySelector('.company-menu').style.display == "block") {
-        document.querySelector('.company-menu').style.display = 'none';
+    if (companyMenu.style.visibility == "visible") {
+        companyMenu.style.visibility = 'hidden';
+        companyMenu.style.opacity = 0;
         companyLi.style.setProperty('--arrow', 'url(./images/icon-arrow-down.svg)');
+        companyMenu.classList.add('shown');
     } else {
-        document.querySelector('.company-menu').style.display = 'block';
+        featuresMenu.style.visibility = 'hidden';
+        companyMenu.style.visibility = 'visible';
+        companyMenu.style.opacity = 1;
         companyLi.style.setProperty('--arrow', 'url(./images/icon-arrow-up.svg)');
+        companyMenu.classList.remove('shown');
     }
 })
 
